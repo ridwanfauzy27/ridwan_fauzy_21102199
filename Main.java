f51c24457ee8ae8366b841846fe76ae76096f5cf
ridwanfauzy27 <132335696+ridwanfauzy27@users.noreply.github.com>
2023-05-03 15:43:56 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,32 @@
-package com.Ridwan.PBO.Pertemuan4;
+package com.Ridwan.PBO.Pertemuan5;
 
 public class Main {
     public static void main(String[] args) {
-        Manager manager = new Manager();
-        Pegawai pegawai = new Pegawai();
+        Barang[] keranjang = new Barang[10];
+        Barang[] etalase = {
+                new Barang("Indomie", 2000),
+                new Barang("Mie Sedap", 2000),
+                new Barang("Beras", 12000),
+                new Barang("Gula", 15000),
+        };
+        System.out.println(
+                "Banyak Barang di Etalase :" + etalase.length
+        );
+        etalase[0].showInfo();
 
-        manager.nip = 181910024;
-        manager.nama = "Basuki";
-
-        pegawai.nip = 191220032;
-        pegawai.nama = "Ahmad";
-
-        manager.showInfo();
-        manager.extraInfo();
-        manager.bonus(1000000);
-
-        pegawai.showInfo();
-        pegawai.extraInfo();
+        System.out.println("Daftar Barang di Etalase : ");
+        int i = 1;
+        for (Barang barang : etalase){
+            System.out.println("Barang ke-" + 1);
+            barang.showInfo();
+            i++;
+        }
+    for (int j=0;j<etalase.length; j++){
+        System.out.println("Barang ke-" + (j+1));
+        etalase[j].showInfo();
+    }
+    keranjang[0] = etalase[0];
+        System.out.println("Barang di keranjang : ");
+        keranjang[0].showInfo();
     }
 }
