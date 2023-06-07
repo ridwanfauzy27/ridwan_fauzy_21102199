2e27b4162004e06882f3b73c79d8877011470dcd
Ridwan_Fauzy <132335696+ridwanfauzy27@users.noreply.github.com>
2023-06-07 11:40:35 +0700

    Add files via upload

---
+++ Register.java
@@ -0,0 +1,19 @@
+package com.Ridwan.PBO.Pertemuan7;
+
+import javax.swing.*;
+
+public class Register {
+    private JTextField ridwanFauzyTextField;
+    private JTextField a21102199TextField;
+    private JComboBox comboBox1;
+    private JTextField a0895386297138TextField;
+    private JRadioButton lakiLakiRadioButton;
+    private JRadioButton perempuanRadioButton;
+    private JTextArea perumPuriIndahBlokTextArea;
+    private JButton SAVEButton;
+    private JButton CLEARButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
